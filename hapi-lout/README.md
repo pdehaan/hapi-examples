4f0994756bebc92ee6ba25d1da3d0c519aea3258
Peter deHaan <peter@deseloper.com>
2014-09-26 16:08:02 -0700

    Adding hapi-lout example

---
+++ hapi-lout/README.md
@@ -0,0 +1,34 @@
+# hapi-lout
+
+The following examplee shows how you can use the [lout](https://github.com/hapijs/lout) API documentation generator with [hapi.js](http://hapijs.com/) to generate a human-readable guide for every endpoint using the route configuration.
+
+## Usage
+
+```sh
+$ npm install
+$ npm start
+```
+
+Once the hapi server is started, open a browser and go to http://localhost:3000/docs to see the generated route documentation.
+
+## Excluding routes
+
+To exclude a route from the lout generated documentation, set `lout` to `false` in the route configuration. For example,
+
+```json
+{
+  method: 'GET',
+  path: '/private',
+  handler: function (req, reply) {
+    reply('private route');
+  },
+  config: {
+    description: '/private route',
+    notes: 'Note for the `/private` route. This documentation should not appear in /docs.',
+    tags: ['private', 'undocumented'],
+    plugins: {
+      lout: false
+    }
+  }
+}
+```
---
+++ hapi-lout/server.js
@@ -0,0 +1,63 @@
+var lout = require('lout');
+var Hapi = require('hapi');
+
+var server = new Hapi.Server(3000);
+
+server.route([
+  {
+    method: 'GET',
+    path: '/',
+    handler: function (req, reply) {
+      reply('hello world');
+    },
+    config: {
+      description: 'Default route',
+      notes: 'Note for the default route',
+      tags: ['default', 'hey world']
+    }
+  }, {
+    method: 'GET',
+    path: '/hello',
+    handler: function (req, reply) {
+      reply('hello');
+    },
+    config: {
+      description: '/hello route',
+      notes: 'Note for the `/hello` route',
+      tags: ['hello']
+    }
+  }, {
+    method: 'GET',
+    path: '/world',
+    handler: function (req, reply) {
+      reply('world');
+    },
+    config: {
+      description: '/world route',
+      notes: 'Note for the `/world` route',
+      tags: ['world']
+    }
+  }, {
+    method: 'GET',
+    path: '/private',
+    handler: function (req, reply) {
+      reply('private route');
+    },
+    config: {
+      description: '/private route',
+      notes: 'Note for the `/private` route. This documentation should not appear in /docs.',
+      tags: ['private', 'undocumented'],
+      plugins: {
+        lout: false
+      }
+    }
+  }
+]);
+
+server.pack.register({
+  plugin: lout
+}, function () {
+  console.log('Hapi %s server running at: %s', Hapi.version, server.info.uri);
+  console.log('To view the generated route documentation, see %s/docs', server.info.uri);
+  server.start();
+});
